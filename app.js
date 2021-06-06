61b9c622dcc336d700f8db0fa1c13bdb37d517ae
Himasha Weerasinghe <himashatw@gmail.com>
2021-06-06 22:59:19 +0530

    updated

--- app.js
+++ app.js
@@ -6,9 +6,7 @@ const PORT = process.env.PORT || 8080
 app.use(express.json())
 
 app.get("/", (req, res) => {
-    res.status(200).json({
-        message: "Hello User"
-    })
+    res.status(200).send("PumCC Pamkaya")
 })
 
 app.listen(PORT, () => {
